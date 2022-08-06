7259311b6d47e54641679bdd23f4c1cd01ca38bd
Haswanth Reddy <v.haswanthreddy@gmail.com>
2022-08-06 09:41:41 +0530

    implementing array

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,4 +1,4 @@
-class Myarray {
+class MyArray {
     constructor () {
         this.length = 0;
         this.data = {};
@@ -27,10 +27,31 @@ class Myarray {
     }
 
     shiftItems (index) {
-        for (const i = index; i < this.length - 1; i++) {
+        for (let i = index; i < this.length - 1; i++) {
             this.data[i] = this.data[i+1]; 
         }
         delete this.data[this.length-1];
         this.length--;
     }
 }
+
+const arr = new MyArray();
+
+arr.push('hi'); // O(1)
+arr.push(1);
+arr.push(5);
+arr.push(true);
+
+console.log(arr)
+
+arr.pop(); // O(1)
+
+console.log(arr)
+
+arr.deleteAtIndex(1); // O(n)
+
+console.log(arr)
+
+arr.shiftItems(1); // O(n)
+
+console.log(arr)
